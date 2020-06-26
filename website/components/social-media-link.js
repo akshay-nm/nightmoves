949b972e045eed6e84fa72a3ba7ed070287f1bec
Akshay Kumar <akshay.kumar@sdiot.io>
2020-06-27 03:16:14 +0530

    Add social-media-link component

---
+++ website/components/social-media-link.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useSpring, animated } from 'react-spring';
+
+const SocialMediaLink = ({ icon, profileLink }) => {
+  const [props, set, stop] = useSpring(() => ({ position: 'absolute', padding: '2rem', opacity: 0, fontSize: 30 }))
+  const [hover, setHover] = useState(false)
+
+  useEffect(() => {
+    set({ opacity: 1 })
+  }, [])
+
+
+  useEffect(() => {
+    set({ fontSize: hover? 40: 30 }) 
+  }, [hover])
+
+  return (
+    <animated.a 
+      href={profileLink}
+      rel='noopener noreferrer'
+      target='_blank'
+      style={props}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)} >
+      <FontAwesomeIcon icon={icon} />
+    </animated.a>
+  );
+};
+
+export default SocialMediaLink;
