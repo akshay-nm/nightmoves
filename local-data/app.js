75407df1207b931c9358a8f20a741f9690cbaa1f
Akshay Kumar <akshay.kumar@sdiot.io>
2020-06-29 19:46:13 +0530

    Setup body-parser

--- local-data/app.js
+++ local-data/app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
 
+app.use(require('body-parser').json())
+
 app.use('/', require('./routes'))
 
 app.listen(port, () => console.log(`Local data server listening at http://localhost:${port}`))
